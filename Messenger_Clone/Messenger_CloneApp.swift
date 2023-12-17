f2ac42544bf7a341fc51e81e20e3978a2524b1fc
levanduy093-work <levanduy.work@gmail.com>
2023-12-18 03:31:13 +0700

    Initial Commit

---
+++ Messenger_Clone/Messenger_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messenger_CloneApp.swift
+//  Messenger_Clone
+//
+//  Created by Lê Văn Duy on 18/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Messenger_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
