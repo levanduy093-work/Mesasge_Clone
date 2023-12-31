4d747ee2074944a5c089f3832ad55d3e2de72445
levanduy093-work <levanduy.work@gmail.com>
2023-12-31 14:08:42 +0700

    Send Message Logic

---
+++ Messenger_Clone/Core/Chat/ViewModel/ChatViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChatViewModel.swift
+//  Messenger_Clone
+//
+//  Created by Lê Văn Duy on 31/12/2023.
+//
+
+import Foundation
---
+++ Messenger_Clone/Model/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  Messenger_Clone
+//
+//  Created by Lê Văn Duy on 31/12/2023.
+//
+
+import Foundation
